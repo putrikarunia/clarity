121b808ccec2f818c2dbacdaac47300b236ce4f9
Putri Karunia <putri@calvisitor-10-105-133-106.calvisitor.1918.berkeley.edu>
2018-11-09 21:51:42 -0800

    added nav bar and home fragment (WIP)

--- app/src/main/java/com/example/clarity/clarity/MainActivity.java
+++ app/src/main/java/com/example/clarity/clarity/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.clarity.clarity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static android.support.design.bottomnavigation.LabelVisibilityMode.LABEL_VISIBILITY_UNLABELED;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigation;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupNavigationView();
     }
+
+
+    private void setupNavigationView() {
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.inflateMenu(R.menu.bottom_navigation_menu);
+        fragmentManager = getSupportFragmentManager();
+
+        //Set Add Menu to be selected by default
+        Menu menu = bottomNavigation.getMenu();
+        menu.getItem(1).setChecked(true);
+        fragment = new AddFragment();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.rootLayout, fragment).commit();
+
+        //Removing labels and animation
+        bottomNavigation.setLabelVisibilityMode(LABEL_VISIBILITY_UNLABELED);
+        bottomNavigation.setItemHorizontalTranslation(false);
+        bottomNavigation.setItemIconTintList(getResources().getColorStateList(R.color.bottom_nav_color));
+
+        //Handle choosing the menu
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.action_gallery:
+                        fragment = new AddFragment();
+                        break;
+                    case R.id.action_add:
+                        fragment = new AddFragment();
+                        break;
+                    case R.id.action_settings:
+                        fragment = new AddFragment();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.rootLayout, fragment).commit();
+                return true;
+            }
+        });
+    }
+
+
 }
