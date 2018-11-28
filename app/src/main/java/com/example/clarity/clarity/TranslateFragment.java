772a8188a0bfc0b750179796cf378d80cd5e1f2a
Morgan <morgan.adams@lifelink.com>
2018-11-28 00:24:35 -0800

    Added arrows to color selection

--- app/src/main/java/com/example/clarity/clarity/TranslateFragment.java
+++ app/src/main/java/com/example/clarity/clarity/TranslateFragment.java
@@ -168,8 +168,7 @@ public class TranslateFragment extends Fragment {
         highlighter = (ImageView) v.findViewById(R.id.highlighter);
         shift = dpToPx(25);         // TODO Change this depending on size of font
         // TODO Update height of highlighter depending on font size as well
-        highlighter.setColorFilter(R.color.highlightBlue); // White Tint
-
+        // TODO Update color of highlighter according to settings
 
 
 
